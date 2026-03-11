f9ae2659b4eec0adc946a562c7ba914948ce57b4
HereIsSee <biramantvydas@gmail.com>
2026-03-11 20:34:00 +0200

    work on dashboard portfolio design

--- app/dashboard/portfolios/page.tsx
+++ app/dashboard/portfolios/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/Button";
+import { Plus } from "lucide-react";
+
 export default function Portfolios() {
-  return <div>Portfolios</div>;
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <div className="flex justify-between">
+        <div className="text-3xl font-semibold">Portfolios</div>
+        <Button>
+          <Plus />
+        </Button>
+      </div>
+    </div>
+  );
 }
